6a00626681b196c810253abb37ca971dbf75276f
Dida1209 <duhuiying1209@Outlook.com>
2017-02-23 22:51:34 +0800

    init

---
+++ app.js
@@ -0,0 +1,46 @@
+/**
+ * Created by lenovo-pc on 2017/2/23.
+ */
+var express = require('express');
+var port = process.env.port || 3000;
+var bodyParser = require('body-parser');
+var path = require('path');
+var logger = require('morgan');
+var app = express();
+
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var mongoStore = require('connect-mongo')(session);
+
+var mongoose = require('mongoose');
+var dbUrl = "mongodb://localhost/graduation";
+mongoose.connect(dbUrl);
+
+app.set('views','./views/pages');
+app.set('view engine','jade');
+app.use(express.static(path.join(__dirname,'/public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(cookieParser());
+app.use(session({
+    secret:'graduation',
+    store:new mongoStore({
+        url:dbUrl,
+        collection:'sessions'
+    })
+}));
+
+if(app.get('env')==='development'){
+    app.set('showStackError',true);
+    app.use(logger('dev'));
+    app.locals.pretty = true;
+    mongoose.set('debug',true);
+}
+
+require('./config/routes')(app);
+app.locals.moment = require('moment');
+app.listen(port);
+
+console.log('success in '+port);
+
---
+++ config/routes.js
@@ -0,0 +1,16 @@
+/**
+ * Created by lenovo-pc on 2017/2/23.
+ */
+//var
+
+
+module.exports = function(app){
+//首页
+    app.get('/',function(req,res){
+        res.render('index',{
+
+        })
+    })
+
+//
+}
